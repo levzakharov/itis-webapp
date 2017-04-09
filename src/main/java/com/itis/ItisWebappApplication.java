580b37f9cf39f58161d090f907df8a872e3ac835
Alexander Pliskin <alexnetmore@gmail.com>
2017-04-09 13:23:25 +0300

    [issue-2] : project initialization, spring-boot dependencies added

---
+++ src/main/java/com/itis/ItisWebappApplication.java
@@ -0,0 +1,12 @@
+package com.itis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItisWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItisWebappApplication.class, args);
+	}
+}
