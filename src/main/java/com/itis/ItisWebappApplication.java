1ed8d9e5c7ec513596cac2798418397df8ef6def
Gatin Marat <marat96g@gmail.com>
2017-05-12 01:25:18 +0300

    [issue/7-login_page] freemarker was added

--- src/main/java/com/itis/ItisWebappApplication.java
+++ src/main/java/com/itis/ItisWebappApplication.java
@@ -1,12 +1,15 @@
 package com.itis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class, org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class})
 public class ItisWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItisWebappApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/itis/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.itis.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by maratgatin on 12/05/2017.
+ */
+@Controller
+public class MainController {
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+}
