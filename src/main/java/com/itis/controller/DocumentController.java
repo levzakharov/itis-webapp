814b0c3658bd5af4acc98748c6a3c348db73063c
SoftIlnyr <softilnyr16@gmail.com>
2017-06-01 03:23:18 +0300

    better design of account management ui

--- src/main/java/com/itis/controller/DocumentController.java
+++ src/main/java/com/itis/controller/DocumentController.java
@@ -49,10 +49,14 @@ public class DocumentController {
     public String getTeacherDocumentsPage(@PathVariable int userId, ModelMap modelMap) {
         boolean isOwner = SecurityUtils.getCurrentUser().getId() == userId;
         User user = userService.getById(userId);
-        modelMap.put("documents", documentService.getByUserId(userId));
-        modelMap.put("isOwner", isOwner);
-        modelMap.put("teacher", user);
-        return "document/docs_teacher";
+        if (user != null && user.getRoles().contains(Role.TEACHER)) {
+            modelMap.put("documents", documentService.getByUserId(userId));
+            modelMap.put("isOwner", isOwner);
+            modelMap.put("teacher", user);
+            return "document/docs_teacher";
+        } else {
+            return "redirect:" + ApplicationUrls.WebAppUrls.TEACHER_FOLDERS_URL;
+        }
     }
 
     @GetMapping(value = ApplicationUrls.WebAppUrls.DEAN_DOCUMENTS_URL)
